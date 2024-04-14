553d1912c6fadbc51369076e7b7f8a949b2330b2
Munieru <20086673+munierujp@users.noreply.github.com>
2024-04-14 12:12:31 +0900

    Create example.ts

---
+++ example/example.ts
@@ -0,0 +1,11 @@
+// Try ESLint on this file
+
+/**
+ * @param userName - user name
+ */
+const sayHello = (userName: string): void => {
+  console.log(`Hello, ${userName}`)
+}
+
+const myName = 'Alice'
+sayHello(myName)
