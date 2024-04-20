989723820d4113f0450889ad8b6d9214352ff251
Munieru <20086673+munierujp@users.noreply.github.com>
2024-04-21 02:13:15 +0900

    Remove disable comments

--- example/example.js
+++ example/example.js
@@ -4,8 +4,6 @@
  * @param {string} userName - user name
  * @returns {void}
  */
-// FIXME: https://github.com/mightyiam/eslint-config-love/issues/1525
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const sayHello = (userName) => {
   console.log(`Hello, ${userName}`)
 }
